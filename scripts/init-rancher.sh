9eb91759d21cfadd3b3b0dc56299272095c4e50d
Adam Toy <adam.toy@rancherfederal.com>
2022-11-07 18:31:41 -0500

    use CF

--- scripts/init-rancher.sh
+++ scripts/init-rancher.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
 RANCHER_HOST=$1
-RKE2_VERSION="v1.23.10+rke2r1"
-
-if [[ -z "$RANCHER_HOST" ]]; then
-    echo "ERROR: Missing 'RANCHER_HOST' environment variable."
-    exit 1
-fi
 
 echo ""
 echo "====================================="
-echo "1) Installing RKE2.."
-echo "====================================="
-curl -sfL https://get.rke2.io | INSTALL_RKE2_VERSION=$RKE2_VERSION sh -
-systemctl enable rke2-server
-systemctl start rke2-server
-
-echo ""
-echo "====================================="
-echo "2) Creating symlink for kubectl.."
+echo "1) Creating symlink for kubectl.."
 echo "====================================="
 while [ ! -f /var/lib/rancher/rke2/bin/kubectl ]
 do
@@ -34,35 +20,56 @@ echo "Waiting for DaemonSet to be created.."
 until KUBECONFIG=/etc/rancher/rke2/rke2.yaml kubectl get daemonset -n kube-system rke2-ingress-nginx-controller > /dev/null 2>&1; do
     sleep 3
 done
-
+  
 echo "Waiting for pods to be ready.."
 until [[ $(KUBECONFIG=/etc/rancher/rke2/rke2.yaml kubectl get ds -n kube-system rke2-ingress-nginx-controller -o json | jq -r '.status.numberReady') == $(KUBECONFIG=/etc/rancher/rke2/rke2.yaml kubectl get ds -n kube-system rke2-ingress-nginx-controller -o json | jq -r '.status.desiredNumberScheduled') ]]; do
     sleep 3
 done
 
-echo ""
-echo "====================================="
-echo "3) Installing cert-manager (v1.7.1).."
-echo "====================================="
-KUBECONFIG=/etc/rancher/rke2/rke2.yaml helm install --namespace cert-manager --create-namespace --set installCRDs=true cert-manager /charts/cert-manager-v1.7.1.tgz
+if [[ $(KUBECONFIG=/etc/rancher/rke2/rke2.yaml helm status -n cert-manager cert-manager -o json 2>/dev/null | jq -r '.info.status') != "deployed" ]]; then
+  echo ""
+  echo "====================================="
+  echo "3) Installing cert-manager (v1.7.1).."
+  echo "====================================="
+  KUBECONFIG=/etc/rancher/rke2/rke2.yaml helm install --namespace cert-manager --create-namespace --set installCRDs=true cert-manager /charts/cert-manager-v1.7.1.tgz
+fi
+
+if [[ $(KUBECONFIG=/etc/rancher/rke2/rke2.yaml helm status -n kube-system aws-cloud-controller-manager -o json 2>/dev/null | jq -r '.info.status') != "deployed" ]]; then
+  echo ""
+  echo "====================================="
+  echo "3) Configuring AWS Cloud Provider.."
+  echo "====================================="
+  KUBECONFIG=/etc/rancher/rke2/rke2.yaml helm install --namespace kube-system --create-namespace aws-cloud-controller-manager /charts/aws-cloud-controller-manager-0.0.7.tgz
+
+  echo ""
+  echo "======================================================="
+  echo "4) Updating native NGINX chart to type 'LoadBalancer'.."
+  echo "======================================================="
+  mv /charts/rke2-ingress-nginx-config.yaml /var/lib/rancher/rke2/server/manifests/rke2-ingress-nginx-config.yaml
+fi
 
 echo ""
 echo "======================================="
-echo "5) Creating 'cattle-system' namespace.."
+echo "5) Creating 'cattle-system' namespace if it doesn't exist.."
 echo "======================================="
-KUBECONFIG=/etc/rancher/rke2/rke2.yaml kubectl create namespace cattle-system
+cat <<EOF | KUBECONFIG=/etc/rancher/rke2/rke2.yaml kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: cattle-system
+  labels:
+    name: cattle-system
+EOF
 
-echo ""
-echo "======================="
-echo "7) Installing Rancher.."
-echo "======================="
-cat <<EOT > /tmp/rancher-values.yaml
+if [[ $(KUBECONFIG=/etc/rancher/rke2/rke2.yaml helm status -n cattle-system rancher -o json 2>/dev/null | jq -r '.info.status') != "deployed" ]]; then
+  echo ""
+  echo "======================="
+  echo "6) Installing Rancher.."
+  echo "======================="
+  cat <<EOT > /tmp/rancher-values.yaml
 hostname: $RANCHER_HOST
-
-replicas: 1
-ingress:
-  extraAnnotations:
-    kubernetes.io/ingress.class: nginx
+replicas: 3
 EOT
 
-KUBECONFIG=/etc/rancher/rke2/rke2.yaml helm install --namespace cattle-system -f /tmp/rancher-values.yaml rancher /charts/rancher-2.6.8.tgz
+  KUBECONFIG=/etc/rancher/rke2/rke2.yaml helm install --namespace cattle-system -f /tmp/rancher-values.yaml rancher /charts/rancher-2.6.8.tgz
+fi
---
+++ scripts/init-rke2.sh
@@ -0,0 +1,156 @@
+#!/bin/sh
+
+export SERVER_TYPE="server"
+export JOIN_TOKEN="$1"
+export SERVER_URL="$2"
+export AWS_DEFAULT_REGION="$3"
+
+# info logs the given argument at info log level.
+info() {
+    echo "[INFO] " "$@"
+}
+
+# warn logs the given argument at warn log level.
+warn() {
+    echo "[WARN] " "$@" >&2
+}
+
+# fatal logs the given argument at fatal log level.
+fatal() {
+    echo "[ERROR] " "$@" >&2
+    exit 1
+}
+
+timestamp() {
+  date "+%Y-%m-%d %H:%M:%S"
+}
+
+config() {
+  mkdir -p "/etc/rancher/rke2"
+  cat <<EOF > "/etc/rancher/rke2/config.yaml"
+tls-san:
+  - ${SERVER_URL}
+token: ${JOIN_TOKEN}
+EOF
+}
+
+append_config() {
+  echo "$1" >> "/etc/rancher/rke2/config.yaml"
+}
+
+# The most simple "leader election" you've ever seen in your life
+elect_leader() {
+  # Fetch other running instances in ASG
+  instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+  asg_name=$(aws autoscaling describe-auto-scaling-instances --instance-ids "$instance_id" --query 'AutoScalingInstances[*].AutoScalingGroupName' --output text)
+  instances=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name "$asg_name" --query 'AutoScalingGroups[*].Instances[?HealthStatus==`Healthy`].InstanceId' --output text)
+
+  # Simply identify the leader as the first of the instance ids sorted alphanumerically
+  leader=$(echo $instances | tr ' ' '\n' | sort -n | head -n1)
+
+  info "Current instance: $instance_id | Leader instance: $leader"
+
+  if [ "$instance_id" = "$leader" ]; then
+    SERVER_TYPE="leader"
+    info "Electing as cluster leader"
+  else
+    info "Electing as joining server"
+  fi
+}
+
+identify() {
+  # Default to server
+  SERVER_TYPE="server"
+
+  TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+  supervisor_status=$(curl --write-out '%{http_code}' -sk --output /dev/null https://${SERVER_URL}:9345/ping)
+
+  if [ "$supervisor_status" -ne 200 ]; then
+    info "API server unavailable, performing simple leader election"
+    elect_leader
+  else
+    info "API server available, identifying as server joining existing cluster"
+  fi
+}
+
+cp_wait() {
+  while true; do
+    supervisor_status=$(curl --write-out '%{http_code}' -sk --output /dev/null https://${SERVER_URL}:9345/ping)
+    if [ "$supervisor_status" -eq 200 ]; then
+      info "Cluster is ready"
+
+      # Let things settle down for a bit, not required
+      # TODO: Remove this after some testing
+      sleep 10
+      break
+    fi
+    info "Waiting for cluster to be ready..."
+    sleep 10
+  done
+}
+
+local_cp_api_wait() {
+  export PATH=$PATH:/var/lib/rancher/rke2/bin
+  export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
+
+  while true; do
+    info "$(timestamp) Waiting for kube-apiserver..."
+    if timeout 1 bash -c "true <>/dev/tcp/localhost/6443" 2>/dev/null; then
+        break
+    fi
+    sleep 5
+  done
+
+  wait $!
+
+  nodereadypath='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'
+  until kubectl get nodes --selector='node-role.kubernetes.io/master' -o jsonpath="$nodereadypath" | grep -E "Ready=True"; do
+    info "$(timestamp) Waiting for servers to be ready..."
+    sleep 5
+  done
+
+  info "$(timestamp) all kube-system deployments are ready!"
+}
+
+
+upload() {
+  # Wait for kubeconfig to exist, then upload to s3 bucket
+  retries=10
+
+  while [ ! -f /etc/rancher/rke2/rke2.yaml ]; do
+    sleep 10
+    if [ "$retries" = 0 ]; then
+      fatal "Failed to create kubeconfig"
+    fi
+    ((retries--))
+  done
+
+  # Replace localhost with server url and upload to s3 bucket
+  sed "s/127.0.0.1/${SERVER_URL}/g" /etc/rancher/rke2/rke2.yaml | aws s3 cp - "s3://${token_bucket}/rke2.yaml" --content-type "text/yaml"
+}
+
+{
+  config
+  identify
+
+  if [ $SERVER_TYPE = "server" ]; then     # additional server joining an existing cluster
+    append_config "server: https://${SERVER_URL}:9345"
+    # Wait for cluster to exist, then init another server
+    cp_wait
+  fi
+
+  systemctl enable rke2-server
+  systemctl daemon-reload
+  systemctl start rke2-server
+
+  export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
+  export PATH=$PATH:/var/lib/rancher/rke2/bin
+
+  if [ $SERVER_TYPE = "leader" ]; then
+    # Upload kubeconfig to s3 bucket
+    # upload
+
+    # For servers, wait for apiserver to be ready before continuing so that `post_userdata` can operate on the cluster
+    local_cp_api_wait
+  fi
+}
